e734ce87e06e73e1df92ca70a1f8fa6f541e56a9
KaueASouza <kaue.souza@sptech.school>
2024-05-29 18:40:25 -0300

    Ligação da API com a tela de cadastro e login

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -57,7 +57,7 @@ function cadastrar(req, res) {
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var marca = req.body.marcaServer;
+    var fkmarca = req.body.fkmarcaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
@@ -69,7 +69,7 @@ function cadastrar(req, res) {
     }  else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, marca)
+        usuarioModel.cadastrar(nome, email, senha, fkmarca)
             .then(
                 function (resultado) {
                     res.json(resultado);
