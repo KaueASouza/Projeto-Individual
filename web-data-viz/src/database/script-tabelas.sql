2f98933794a98786b9ce2f41da64afb36be36f3f
unset <unset>
2024-05-30 17:51:57 -0300

    Alteração de icones script do banco de dados

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -10,50 +10,106 @@ CREATE DATABASE aquatech;
 
 USE aquatech;
 
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
-);
+
+CREATE TABLE marca(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome varchar(45));
+
+INSERT INTO marca (nome) VALUES 
+    ('Avelloz'),
+    ('BMW'),
+    ('Dafra'),
+    ('Ducati'),
+    ('Haojue'),
+    ('Harley-Davidson'),
+    ('Honda'),
+    ('Kawasaki'),
+    ('KTM'),
+    ('Kymco'),
+    ('MXF'),
+    ('Piaggio'),
+    ('Pro Tork'),
+    ('Royal Enfield'),
+    ('Shineray'),
+    ('Sousa Motos'),
+    ('Suzuki'),
+    ('Triumph'),
+    ('Voltz Motors'),
+    ('Yamaha'),
+    ('Bajaj'),
+    ('Fantic'),
+    ('MV Agusta'),
+    ('Zontes'),
+    ('Mottu'),
+    ('Watts'),
+    ('Origem'),
+    ('Bull Motors');
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    fkmarca int, 
+    foreign key (fkmarca) references marca(id)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+SELECT * FROM usuario;
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
+(SELECT 'Avelloz' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '1'
+UNION ALL
+SELECT 'BMW' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '2'
+UNION ALL
+SELECT 'Dafra' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '3'
+UNION ALL
+SELECT 'Ducati' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '4'
+UNION ALL
+SELECT 'Haojue' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '5'
+UNION ALL
+SELECT 'Harley-Davidson' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '6'
+UNION ALL
+SELECT 'Honda' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '7'
+UNION ALL
+SELECT 'Kawasaki' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '8'
+UNION ALL
+SELECT 'KTM' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '9'
+UNION ALL
+SELECT 'Kymco' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '10'
+UNION ALL
+SELECT 'MXF' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '11'
+UNION ALL
+SELECT 'Piaggio' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '12'
+UNION ALL
+SELECT 'Pro Tork' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '13'
+UNION ALL
+SELECT 'Royal Enfield' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '14'
+UNION ALL
+SELECT 'Shineray' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '15'
+UNION ALL
+SELECT 'Sousa Motos' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '16'
+UNION ALL
+SELECT 'Suzuki' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '17'
+UNION ALL
+SELECT 'Triumph' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '18'
+UNION ALL
+SELECT 'Voltz Motors' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '19'
+UNION ALL
+SELECT 'Yamaha' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '20'
+UNION ALL
+SELECT 'Bajaj' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '21'
+UNION ALL
+SELECT 'Fantic' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '22'
+UNION ALL
+SELECT 'MV Agusta' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '23'
+UNION ALL
+SELECT 'Zontes' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '24'
+UNION ALL
+SELECT 'Mottu' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '25'
+UNION ALL
+SELECT 'Watts' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '26'
+UNION ALL
+SELECT 'Origem' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '27'
+UNION ALL
+SELECT 'Bull Motors' as marca, count(*) as qtd FROM usuario WHERE fkmarca = '28');
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+select * from usuario;
